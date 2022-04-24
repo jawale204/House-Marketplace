fd1d2bc5b7c6fc1213c2e866a009a4e4316c1fc5
jawale204 <jawale204@gmail.com>
2022-04-24 11:25:08 +0530

    Explore component added 15-94

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explore() {
-  return <div>Explore</div>;
+  return (
+    <>
+      <div className="explore">
+        <header>
+          <p className="pageHeader">Explore</p>
+        </header>
+        <main>
+          <p className="exploreCategoryHeading">Category</p>
+          <div className="exploreCategories">
+            <Link to="/category/rent">
+              <img
+                src={rentCategoryImage}
+                alt="Rent Category"
+                className="exploreCategoryImg"
+              />
+              <p className="exploreCategoryName">Place for rent</p>
+            </Link>
+            <Link to="/category/sell">
+              <img
+                src={sellCategoryImage}
+                alt="Sell Category"
+                className="exploreCategoryImg"
+              />
+              <p className="exploreCategoryName">Place for sell</p>
+            </Link>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default Explore;
